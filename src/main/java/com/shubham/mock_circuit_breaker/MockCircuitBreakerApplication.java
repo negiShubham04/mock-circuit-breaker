13d96ffdafe84d0fa8ba31f33cb2183a8fca93c5
shubham negi <shubhamnegi1704v2.0@gmail.com>
2026-01-30 23:32:55 +0530

    init

---
+++ src/main/java/com/shubham/mock_circuit_breaker/MockCircuitBreakerApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.mock_circuit_breaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockCircuitBreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockCircuitBreakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubham/mock_circuit_breaker/MockCircuitBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.mock_circuit_breaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockCircuitBreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
